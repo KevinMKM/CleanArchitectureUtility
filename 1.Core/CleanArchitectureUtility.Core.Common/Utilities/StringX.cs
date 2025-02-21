a79185391870a6a99bcc022b5ab2e660c918788a
Kevin <M.Koorani.Airtour@Gmail.com>
2025-02-21 15:24:58 +0330

    add some extensions for work with strings

---
+++ 1.Core/CleanArchitectureUtility.Core.Common/Utilities/StringX.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArchitectureUtility.Core.Common.Utilities
+{
+    public static class StringX
+    {
+        public static bool IsLengthBetween(this string source, int min, int max)
+            => source.Length >= min && source.Length <= max;
+
+        public static string AggregateX(this List<string>? source, string seed)
+            => source == null || !source.Any() ? string.Empty : source.Aggregate((s1, s2) => $"{s1}{seed}{s2}");
+
+        public static string AggregateX(this IEnumerable<string>? source, string seed)
+            => source?.ToList().AggregateX(seed);
+    }
+}
